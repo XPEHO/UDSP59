01bfe42b5a585437df5d0eda7e2b978330564ddc
Ryan Debouvries <ryandebouvries@hotmail.fr>
2023-10-30 17:17:36 +0100

    test(unit_tests): Add tests and fix some troubles

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -17,58 +17,62 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    modules = ModalRoute.of(context)!.settings.arguments as List<Module>;
+    if (ModalRoute.of(context)!.settings.arguments != null) {
+      modules = ModalRoute.of(context)!.settings.arguments as List<Module>;
+    }
 
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            const TitleHeader(),
-            const SizedBox(
-              height: 40,
-            ),
-            Text(
-              tr("homeEmergency"),
-              style: textStyleHookTitle(),
-            ),
-            const SizedBox(
-              height: 5,
-            ),
-            Text(
-              tr("homeClickEmergency"),
-              textAlign: TextAlign.center,
-              style: textStyleHookSubtitle(),
-            ),
-            const SizedBox(
-              height: 40,
-            ),
-            const Placeholder(
-              fallbackHeight: 180,
-            ),
-            const SizedBox(
-              height: 40,
-            ),
-            RichText(
-              text: TextSpan(
-                text: tr("homeWaiting"),
-                style: textStylePageSubtitle(),
-                children: [
-                  TextSpan(
-                    text: " ${tr("homeWaitingAct")}",
-                    style: textStylePageSubtitleImportant(),
-                  ),
-                  const TextSpan(
-                    text: " !",
-                  ),
-                ],
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const TitleHeader(),
+              const SizedBox(
+                height: 40,
               ),
-              textAlign: TextAlign.center,
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-            if (modules != []) ModulesCarousel(modules: modules),
-          ],
+              Text(
+                tr("homeEmergency"),
+                style: textStyleHookTitle(),
+              ),
+              const SizedBox(
+                height: 5,
+              ),
+              Text(
+                tr("homeClickEmergency"),
+                textAlign: TextAlign.center,
+                style: textStyleHookSubtitle(),
+              ),
+              const SizedBox(
+                height: 40,
+              ),
+              const Placeholder(
+                fallbackHeight: 180,
+              ),
+              const SizedBox(
+                height: 40,
+              ),
+              RichText(
+                text: TextSpan(
+                  text: tr("homeWaiting"),
+                  style: textStylePageSubtitle(),
+                  children: [
+                    TextSpan(
+                      text: " ${tr("homeWaitingAct")}",
+                      style: textStylePageSubtitleImportant(),
+                    ),
+                    TextSpan(
+                      text: " ${tr("homeWaitingExclamation")}",
+                    ),
+                  ],
+                ),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              if (modules != []) ModulesCarousel(modules: modules),
+            ],
+          ),
         ),
       ),
     );
