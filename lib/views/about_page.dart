434ec9542fb1105c8873686ed0bef1fa6ac9229f
Ryan Debouvries <ryandebouvries@hotmail.fr>
2023-10-30 17:17:36 +0100

    test(unit_tests): Add tests and fix some troubles

--- lib/views/about_page.dart
+++ lib/views/about_page.dart
@@ -14,115 +14,117 @@ class AboutPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const PageHeader(
-              pageTitle: 'aboutTitle',
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    tr('aboutSubtitle1'),
-                    style: textStylePageSubtitle(),
-                  ),
-                  const SizedBox(
-                    height: 15,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 10),
-                    child: Column(
-                      children: [
-                        MixedParagraph(context, textKeys: const [
-                          'aboutPart1Span1',
-                          'aboutPart1Span2_b',
-                          'aboutPart1Span3',
-                          'aboutPart1Span4_sb',
-                          'aboutPart1Span5_s',
-                          'aboutPart1Span6_sb',
-                          'aboutPart1Span7',
-                        ]),
-                        const SizedBox(
-                          height: 10,
-                        ),
-                        MixedParagraph(context, textKeys: const [
-                          'aboutPart2Span1',
-                          'aboutPart2Span2_sb',
-                          'aboutPart2Span3_s',
-                          'aboutPart2Span4_sb',
-                          'aboutPart2Span5',
-                          'aboutPart2Span6_sb',
-                          'aboutPart2Span7',
-                        ]),
-                        const SizedBox(
-                          height: 10,
-                        ),
-                        MixedParagraph(context, textKeys: const [
-                          'aboutPart3Span1',
-                          'aboutPart3Span2_b',
-                          'aboutPart3Span3_s',
-                          'aboutPart3Span4_sb',
-                          'aboutPart3Span5_s',
-                        ]),
-                      ],
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  Text(
-                    tr('aboutSubtitle2'),
-                    style: textStylePageSubtitle(),
-                  ),
-                  const SizedBox(
-                    height: 15,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      UrlLinkedIcon(
-                        context,
-                        icon: FontAwesomeIcons.facebookF,
-                        url:
-                            'https://www.facebook.com/people/UDSP59-Formation/100076222514271/',
-                        size: 30,
-                      ),
-                      UrlLinkedIcon(
-                        context,
-                        icon: FontAwesomeIcons.instagram,
-                        url:
-                            'https://www.instagram.com/udsp59formation?igshid=MzRIODBiNWFIZA==',
-                        size: 35,
-                      ),
-                      UrlLinkedIcon(
-                        context,
-                        icon: FontAwesomeIcons.paperPlane,
-                        url: 'mailto:contact@udsp59formation.fr',
-                        size: 30,
-                      ),
-                      UrlLinkedIcon(
-                        context,
-                        icon: FontAwesomeIcons.phone,
-                        url: 'tel:112',
-                        size: 30,
-                      ),
-                      UrlLinkedIcon(
-                        context,
-                        icon: Icons.language,
-                        url: 'https://udsp59formation.fr/',
-                        size: 35,
-                      ),
-                    ],
-                  )
-                ],
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const PageHeader(
+                pageTitle: 'aboutTitle',
               ),
-            ),
-          ],
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      tr('aboutSubtitle1'),
+                      style: textStylePageSubtitle(),
+                    ),
+                    const SizedBox(
+                      height: 15,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 10),
+                      child: Column(
+                        children: [
+                          MixedParagraph(context, textKeys: const [
+                            'aboutPart1Span1',
+                            'aboutPart1Span2_b',
+                            'aboutPart1Span3',
+                            'aboutPart1Span4_sb',
+                            'aboutPart1Span5_s',
+                            'aboutPart1Span6_sb',
+                            'aboutPart1Span7',
+                          ]),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          MixedParagraph(context, textKeys: const [
+                            'aboutPart2Span1',
+                            'aboutPart2Span2_sb',
+                            'aboutPart2Span3_s',
+                            'aboutPart2Span4_sb',
+                            'aboutPart2Span5',
+                            'aboutPart2Span6_sb',
+                            'aboutPart2Span7',
+                          ]),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          MixedParagraph(context, textKeys: const [
+                            'aboutPart3Span1',
+                            'aboutPart3Span2_b',
+                            'aboutPart3Span3_s',
+                            'aboutPart3Span4_sb',
+                            'aboutPart3Span5_s',
+                          ]),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Text(
+                      tr('aboutSubtitle2'),
+                      style: textStylePageSubtitle(),
+                    ),
+                    const SizedBox(
+                      height: 15,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        UrlLinkedIcon(
+                          context,
+                          icon: FontAwesomeIcons.facebookF,
+                          url:
+                              'https://www.facebook.com/people/UDSP59-Formation/100076222514271/',
+                          size: 30,
+                        ),
+                        UrlLinkedIcon(
+                          context,
+                          icon: FontAwesomeIcons.instagram,
+                          url:
+                              'https://www.instagram.com/udsp59formation?igshid=MzRIODBiNWFIZA==',
+                          size: 35,
+                        ),
+                        UrlLinkedIcon(
+                          context,
+                          icon: FontAwesomeIcons.paperPlane,
+                          url: 'mailto:contact@udsp59formation.fr',
+                          size: 30,
+                        ),
+                        UrlLinkedIcon(
+                          context,
+                          icon: FontAwesomeIcons.phone,
+                          url: 'tel:112',
+                          size: 30,
+                        ),
+                        UrlLinkedIcon(
+                          context,
+                          icon: Icons.language,
+                          url: 'https://udsp59formation.fr/',
+                          size: 35,
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,81 +6,164 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:easy_localization/easy_localization.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:udsp59/features/modules_carousel.dart';
+import 'package:udsp59/features/page_header.dart';
 import 'package:udsp59/features/title_header.dart';
+import 'package:udsp59/views/about_page.dart';
 import 'package:udsp59/views/home_page.dart';
 
-void main() {
+void main() async {
+  // Check Easy Localization is initialized
+  SharedPreferences.setMockInitialValues({});
+  await EasyLocalization.ensureInitialized();
+
+  /* Widget initWidgetToTest(widgetToTest) {
+    return EasyLocalization(
+      supportedLocales: const [
+        Locale('fr'),
+      ],
+      path: 'assets/translations',
+      fallbackLocale: const Locale('fr'),
+      child: Builder(
+        builder: (context) {
+          return MaterialApp(
+            locale: context.locale,
+            supportedLocales: context.supportedLocales,
+            localizationsDelegates: context.localizationDelegates,
+            home: Scaffold(
+              body: widgetToTest,
+            ),
+          );
+        },
+      ),
+    );
+  } */
+
   // -----------------------------HOMEPAGE----------------------------------- //
-  testWidgets('HomePage has a TitleHeader, some Texts and a ModuleCarousel',
-      (WidgetTester tester) async {
-    await tester.pumpWidget(const MaterialApp(
-      home: Scaffold(
-        body: HomePage(),
-      ),
-    ));
+  group('Homepage tests', () {
+    testWidgets('HomePage has a TitleHeader, some Texts and a ModuleCarousel',
+        (WidgetTester tester) async {
+      // Find the widget
+      /* await tester.pumpWidget(initWidgetToTest(const HomePage())); */
 
-    // Waiting a second to have translations loaded
-    tester.pumpAndSettle(
-      const Duration(
-        seconds: 1,
-      ),
-    );
+      await tester.pumpWidget(
+        const MaterialApp(
+          home: Scaffold(
+            body: HomePage(),
+          ),
+        ),
+      );
 
-    // Test the presence of the TitleHeader
-    var titleHeader = find.byElementType(TitleHeader);
-    expect(titleHeader, findsOneWidget);
+      // Waiting a second to have translations loaded
+      await tester.pump(const Duration(seconds: 1));
 
-    // Test the presence of the Texts
-    var homeEmergency = find.text(tr("homeEmergency"));
-    expect(homeEmergency, findsOneWidget);
-    var homeClickEmergency = find.text(tr("homeClickEmergency"));
-    expect(homeClickEmergency, findsOneWidget);
-    var homeWaiting = find.text(tr("homeWaiting"));
-    expect(homeWaiting, findsOneWidget);
-    var homeWaitingAct = find.text(tr("homeWaitingAct"));
-    expect(homeWaitingAct, findsOneWidget);
-    var homeWaitingExclamation = find.text(tr("homeWaitingExclamation"));
-    expect(homeWaitingExclamation, findsOneWidget);
+      // Test the presence of the TitleHeader
+      var titleHeader = find.byType(TitleHeader);
+      expect(titleHeader, findsOneWidget);
 
-    // Test the presence of the ModuleCarousel
-    var moduleCarousel = find.byType(ModulesCarousel);
-    expect(moduleCarousel, findsOneWidget);
-  });
+      // Test the presence of the Texts
+      var homeEmergency = find.text(tr("homeEmergency"));
+      expect(homeEmergency, findsOneWidget);
+      var homeClickEmergency = find.text(tr("homeClickEmergency"));
+      expect(homeClickEmergency, findsOneWidget);
+      var homeWaiting = find.textContaining(
+        "${tr("homeWaiting")} ${tr("homeWaitingAct")} ${tr("homeWaitingExclamation")}",
+        findRichText: true,
+      );
+      expect(homeWaiting, findsOneWidget);
 
-  testWidgets('TitleHeader has a Text with the app name, an Image and a Button',
-      (WidgetTester tester) async {
-    await tester.pumpWidget(const MaterialApp(
-      home: Scaffold(
-        body: TitleHeader(),
-      ),
-    ));
+      // Test the presence of the ModuleCarousel
+      var moduleCarousel = find.byType(ModulesCarousel);
+      expect(moduleCarousel, findsOneWidget);
+    });
 
-    // Waiting a second to have translations loaded
-    tester.pumpAndSettle(
-      const Duration(
-        seconds: 1,
-      ),
-    );
+    testWidgets(
+        'TitleHeader has a Text with the app name, an Image and a Button which navigates to the AboutPage',
+        (WidgetTester tester) async {
+      // Find the widget
+      /* await tester.pumpWidget(
+      initWidgetToTest(const TitleHeader()),
+    ); */
 
-    // Test the presence of the logo
-    var appLogo = find.byType(Image);
-    expect(appLogo, findsOneWidget);
+      await tester.pumpWidget(
+        MaterialApp(
+          home: const Scaffold(
+            body: TitleHeader(),
+          ),
+          routes: {
+            '/about': (context) => const AboutPage(),
+          },
+        ),
+      );
 
-    // Test the presence of the app name
-    var appTitle = find.text(tr("appName"));
-    expect(appTitle, findsOneWidget);
+      // Waiting a second to have translations loaded
+      await tester.pump(const Duration(seconds: 1));
 
-    // Test the presence of the about button
-    var aboutButton = find.byIcon(Icons.info_rounded);
-    expect(aboutButton, findsOneWidget);
+      // Test the presence of the logo
+      var appLogo = find.image(const AssetImage('assets/logo.png'));
+      expect(appLogo, findsOneWidget);
 
-    // Test the onPressed callback of the button
+      // Test the presence of the app name
+      var appTitle = find.text(tr("appName"));
+      expect(appTitle, findsOneWidget);
+
+      // Test the presence of the about button
+      var aboutButton = find.widgetWithIcon(TextButton, Icons.info_rounded);
+      expect(aboutButton, findsOneWidget);
+
+      // Test the onPressed navigation of the button
+      await tester.tap(aboutButton);
+      await tester.pumpAndSettle();
+
+      expect(find.byType(AboutPage), findsOneWidget);
+    });
   });
 
   // ----------------------------ABOUT PAGE---------------------------------- //
+  group('About Page tests', () {
+    testWidgets('About Page has a PageHeader, some Texts and some Linked Icons',
+        (WidgetTester tester) async {
+      // Find the widget
+      await tester.pumpWidget(
+        const MaterialApp(
+          home: Scaffold(
+            body: AboutPage(),
+          ),
+        ),
+      );
+
+      // Waiting a second to have translations loaded
+      await tester.pump(const Duration(seconds: 1));
+
+      // Test the presence of the PageHeader
+      var pageHeader = find.byType(PageHeader);
+      expect(pageHeader, findsOneWidget);
+
+      // Test the presence of the Texts
+      var aboutSubtitle1 = find.text(tr("aboutSubtitle1"));
+      expect(aboutSubtitle1, findsOneWidget);
+      var aboutSubtitle2 = find.text(tr("aboutSubtitle2"));
+      expect(aboutSubtitle2, findsOneWidget);
+      var aboutPart1 = find.textContaining(
+        "${tr("aboutPart1Span1")}${tr("aboutPart1Span2_b")}${tr("aboutPart1Span3")} ${tr("aboutPart1Span4_sb")} ${tr("aboutPart1Span5_s")} ${tr("aboutPart1Span6_sb")}${tr("aboutPart1Span7")}",
+        findRichText: true,
+      );
+      expect(aboutPart1, findsOneWidget);
+      var aboutPart2 = find.textContaining(
+        "${tr("aboutPart2Span1")} ${tr("aboutPart2Span2_sb")} ${tr("aboutPart2Span3_s")} ${tr("aboutPart2Span4_sb")}${tr("aboutPart2Span5")} ${tr("aboutPart2Span6_sb")}${tr("aboutPart2Span7")}",
+        findRichText: true,
+      );
+      expect(aboutPart2, findsOneWidget);
+      var aboutPart3 = find.textContaining(
+        "${tr("aboutPart3Span1")}${tr("aboutPart3Span2_b")} ${tr("aboutPart3Span3_s")} ${tr("aboutPart3Span4_sb")} ${tr("aboutPart3Span5_s")}",
+        findRichText: true,
+      );
+      expect(aboutPart3, findsOneWidget);
+    });
+  });
 
   // ----------------------------MODULE PAGE--------------------------------- //
 }
