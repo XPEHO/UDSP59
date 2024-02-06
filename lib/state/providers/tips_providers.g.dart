a02a25328f20a2be76cc0a78524ec0d2ee40f0aa
Ryan Debouvries <debouvriesryan@gmail.com>
2024-02-06 14:52:58 +0100

    feat(firebase): Connect app to firebase and fetch tips to display them

---
+++ lib/state/providers/tips_providers.g.dart
@@ -0,0 +1,38 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'tips_providers.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$tipsHash() => r'9607455cd39b4b2765df25e4fa990cf9697f1507';
+
+/// See also [tips].
+@ProviderFor(tips)
+final tipsProvider = AutoDisposeFutureProvider<List<String>>.internal(
+  tips,
+  name: r'tipsProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$tipsHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef TipsRef = AutoDisposeFutureProviderRef<List<String>>;
+String _$randomTipHash() => r'a84b1f01fb3249c37810c5d56269452bc6eecfec';
+
+/// See also [randomTip].
+@ProviderFor(randomTip)
+final randomTipProvider = AutoDisposeProvider<String>.internal(
+  randomTip,
+  name: r'randomTipProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$randomTipHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef RandomTipRef = AutoDisposeProviderRef<String>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
