491309af5bce7aeb547117b995af91c1a3c563e3
Ryan Debouvries <debouvriesryan@gmail.com>
2024-02-06 16:59:34 +0100

    feat(firebase_modules): Fetch modules from firebase

---
+++ lib/state/providers/modules_providers.g.dart
@@ -0,0 +1,24 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'modules_providers.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$modulesHash() => r'f4c41ad0001875ba6b7c03b3fa2faa7a0bc23664';
+
+/// See also [modules].
+@ProviderFor(modules)
+final modulesProvider = AutoDisposeFutureProvider<List<Module>>.internal(
+  modules,
+  name: r'modulesProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$modulesHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef ModulesRef = AutoDisposeFutureProviderRef<List<Module>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
