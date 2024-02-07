76ceff7fc49998e4cb1d353bb76972cb8d9c1c57
Ryan Debouvries <debouvriesryan@gmail.com>
2024-02-07 10:50:10 +0100

    feat(drag_reload): Add a drag to reload from the top of the app

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:udsp59/entities/module.dart';
@@ -6,6 +7,7 @@ import 'package:udsp59/features/modules_carousel.dart';
 import 'package:udsp59/features/tips_card_switcher.dart';
 import 'package:udsp59/features/title_header.dart';
 import 'package:udsp59/state/providers/modules_providers.dart';
+import 'package:udsp59/state/providers/tips_providers.dart';
 import 'package:udsp59/styles/form_factor.dart';
 import 'package:udsp59/styles/text_style.dart';
 import 'package:url_launcher/url_launcher.dart';
@@ -26,116 +28,153 @@ class HomePage extends ConsumerWidget {
 
     return Scaffold(
       body: SafeArea(
-        child: SingleChildScrollView(
-          child: Stack(
-            children: [
-              Container(
-                decoration: const BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage("assets/background.png"),
-                    fit: BoxFit.cover,
-                    alignment: Alignment.center,
-                  ),
-                ),
-                padding: EdgeInsets.only(
-                    bottom: MediaQuery.of(context).size.width >
-                            FormFactor.tightPhone
-                        ? 110
-                        : 130),
-                constraints: BoxConstraints(
-                  minHeight: MediaQuery.of(context).size.height -
-                      MediaQuery.of(context).padding.top,
-                ),
-                child: Column(
-                  mainAxisSize: MainAxisSize.max,
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    const TitleHeader(),
-                    const SizedBox(
-                      height: 30,
-                    ),
-                    RichText(
-                      text: TextSpan(
-                        text: tr("homeHook"),
-                        style: textStyleSubtitle(context),
-                        children: [
-                          TextSpan(
-                            text: " ${tr("homeHookPlus")}",
-                            style: textStyleSubtitleMore(context),
-                          ),
-                          TextSpan(
-                            text: " ${tr("homeHookExclamation")}",
-                          ),
-                        ],
-                      ),
-                      textAlign: TextAlign.center,
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    if (modules != []) ModulesCarousel(modules: modules),
-                    const SizedBox(
-                      height: 30,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 20),
-                      child: Text(
-                        tr("homeLearn"),
-                        style: textStyleSubtitleLess(context),
-                        textAlign: TextAlign.center,
+        child: ScrollConfiguration(
+          behavior: const ScrollBehavior()
+              .copyWith(physics: const BouncingScrollPhysics(), dragDevices: {
+            PointerDeviceKind.touch,
+            PointerDeviceKind.mouse,
+            PointerDeviceKind.trackpad,
+          }),
+          child: RefreshIndicator(
+            onRefresh: () async {
+              return Future.delayed(const Duration(seconds: 1), () {
+                ref.invalidate(tipsProvider);
+                ref.invalidate(modulesProvider);
+              });
+            },
+            color: Colors.red,
+            child: SingleChildScrollView(
+              physics: const AlwaysScrollableScrollPhysics(),
+              child: Stack(
+                children: [
+                  Container(
+                    decoration: const BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage("assets/background.png"),
+                        fit: BoxFit.cover,
+                        alignment: Alignment.center,
                       ),
                     ),
-                    const SizedBox(
-                      height: 20,
+                    padding: EdgeInsets.only(
+                        bottom: MediaQuery.of(context).size.width >
+                                FormFactor.tightPhone
+                            ? 110
+                            : 130),
+                    constraints: BoxConstraints(
+                      minHeight: MediaQuery.of(context).size.height -
+                          MediaQuery.of(context).padding.top,
                     ),
-                    const TipsCardSwitcher(),
-                  ],
-                ),
-              ),
-              Positioned(
-                bottom: 10,
-                width: MediaQuery.of(context).size.width,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Flex(
-                      direction: MediaQuery.of(context).size.width >
-                              FormFactor.tightPhone
-                          ? Axis.horizontal
-                          : Axis.vertical,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      mainAxisSize: MainAxisSize.min,
+                    child: Column(
+                      mainAxisSize: MainAxisSize.max,
+                      mainAxisAlignment: MainAxisAlignment.start,
                       children: [
-                        TextButton(
-                          onPressed: () async {
-                            String url =
-                                'https://github.com/XPEHO/UDSP59/blob/main/PRIVACY_POLICY.md';
-                            final Uri uri = Uri.parse(url);
-                            if (await canLaunchUrl(uri)) {
-                              if (!await launchUrl(
-                                uri,
-                                mode: LaunchMode.externalApplication,
-                              )) {
-                                throw Exception('Could not launch $url');
-                              }
-                            }
-                          },
+                        const TitleHeader(),
+                        const SizedBox(
+                          height: 30,
+                        ),
+                        RichText(
+                          text: TextSpan(
+                            text: tr("homeHook"),
+                            style: textStyleSubtitle(context),
+                            children: [
+                              TextSpan(
+                                text: " ${tr("homeHookPlus")}",
+                                style: textStyleSubtitleMore(context),
+                              ),
+                              TextSpan(
+                                text: " ${tr("homeHookExclamation")}",
+                              ),
+                            ],
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        if (modules != []) ModulesCarousel(modules: modules),
+                        const SizedBox(
+                          height: 30,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 20),
                           child: Text(
-                            tr("privacyPolicy"),
+                            tr("homeLearn"),
+                            style: textStyleSubtitleLess(context),
                             textAlign: TextAlign.center,
-                            style: textStyleFooterLink(context),
                           ),
                         ),
-                        if (MediaQuery.of(context).size.width >
-                            FormFactor.tightPhone)
-                          Text(
-                            "·",
-                            textAlign: TextAlign.center,
-                            style: textStyleFooterText(context),
-                          ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        const TipsCardSwitcher(),
+                      ],
+                    ),
+                  ),
+                  Positioned(
+                    bottom: 10,
+                    width: MediaQuery.of(context).size.width,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Flex(
+                          direction: MediaQuery.of(context).size.width >
+                                  FormFactor.tightPhone
+                              ? Axis.horizontal
+                              : Axis.vertical,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            TextButton(
+                              onPressed: () async {
+                                String url =
+                                    'https://github.com/XPEHO/UDSP59/blob/main/PRIVACY_POLICY.md';
+                                final Uri uri = Uri.parse(url);
+                                if (await canLaunchUrl(uri)) {
+                                  if (!await launchUrl(
+                                    uri,
+                                    mode: LaunchMode.externalApplication,
+                                  )) {
+                                    throw Exception('Could not launch $url');
+                                  }
+                                }
+                              },
+                              child: Text(
+                                tr("privacyPolicy"),
+                                textAlign: TextAlign.center,
+                                style: textStyleFooterLink(context),
+                              ),
+                            ),
+                            if (MediaQuery.of(context).size.width >
+                                FormFactor.tightPhone)
+                              Text(
+                                "·",
+                                textAlign: TextAlign.center,
+                                style: textStyleFooterText(context),
+                              ),
+                            TextButton(
+                              onPressed: () async {
+                                String url = 'https://udsp59formation.fr/';
+                                final Uri uri = Uri.parse(url);
+                                if (await canLaunchUrl(uri)) {
+                                  if (!await launchUrl(
+                                    uri,
+                                    mode: LaunchMode.externalApplication,
+                                  )) {
+                                    throw Exception('Could not launch $url');
+                                  }
+                                }
+                              },
+                              child: Text(
+                                tr("contactUs"),
+                                textAlign: TextAlign.center,
+                                style: textStyleFooterLink(context),
+                              ),
+                            ),
+                          ],
+                        ),
                         TextButton(
                           onPressed: () async {
-                            String url = 'https://udsp59formation.fr/';
+                            String url = 'https://xpeho.com';
                             final Uri uri = Uri.parse(url);
                             if (await canLaunchUrl(uri)) {
                               if (!await launchUrl(
@@ -146,47 +185,28 @@ class HomePage extends ConsumerWidget {
                               }
                             }
                           },
-                          child: Text(
-                            tr("contactUs"),
+                          child: RichText(
+                            text: TextSpan(
+                              children: [
+                                TextSpan(
+                                  text: tr("byXpeho"),
+                                  style: textStyleFooterText(context),
+                                ),
+                                TextSpan(
+                                  text: "XPEHO",
+                                  style: textStyleOwner(context),
+                                ),
+                              ],
+                            ),
                             textAlign: TextAlign.center,
-                            style: textStyleFooterLink(context),
                           ),
                         ),
                       ],
                     ),
-                    TextButton(
-                      onPressed: () async {
-                        String url = 'https://xpeho.com';
-                        final Uri uri = Uri.parse(url);
-                        if (await canLaunchUrl(uri)) {
-                          if (!await launchUrl(
-                            uri,
-                            mode: LaunchMode.externalApplication,
-                          )) {
-                            throw Exception('Could not launch $url');
-                          }
-                        }
-                      },
-                      child: RichText(
-                        text: TextSpan(
-                          children: [
-                            TextSpan(
-                              text: tr("byXpeho"),
-                              style: textStyleFooterText(context),
-                            ),
-                            TextSpan(
-                              text: "XPEHO",
-                              style: textStyleOwner(context),
-                            ),
-                          ],
-                        ),
-                        textAlign: TextAlign.center,
-                      ),
-                    ),
-                  ],
-                ),
-              )
-            ],
+                  ),
+                ],
+              ),
+            ),
           ),
         ),
       ),
