0b71678a76c97753add1b6817fc57093d976fc1a
Ryan Debouvries <debouvriesryan@gmail.com>
2024-02-15 11:37:16 +0100

    feat(no_screenshots): Disable screenshots on android and ios device by native way

--- android/app/src/main/kotlin/com/xpeho/udsp59/MainActivity.kt
+++ android/app/src/main/kotlin/com/xpeho/udsp59/MainActivity.kt
@@ -1,6 +1,14 @@
 package com.xpeho.udsp59
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import android.view.WindowManager.LayoutParams
 
 class MainActivity: FlutterActivity() {
+
+    // Disable screenshots
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        window.addFlags(LayoutParams.FLAG_SECURE)
+    }
 }
--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,7 +7,23 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    // Hide screen during screenshots
+    self.window.makeSecure()
+    
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+}
+
+// Hide screen during screenshots
+extension UIWindow {
+  func makeSecure () {
+    let field = UITextField()
+    field.isSecureTextEntry = true
+    self.addSubview(field)
+    field.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
+    field.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+    self.layer.superlayer?.addSublayer(field.layer)
+    field.layer.sublayers?.first?.addSublayer(self.layer)
   }
 }
