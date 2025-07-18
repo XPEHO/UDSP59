fd1f796d6f1cac3197087a7f75bd1797f99f3941
Théo Lebègue <theo.lebegue@icloud.com>
2025-07-18 16:44:47 +0200

    feat(dotStepper): add dot stepper horizontal module scroll

---
+++ lib/features/dots_stepper.dart
@@ -0,0 +1,153 @@
+import 'package:flutter/material.dart';
+
+class DotsStepper extends StatefulWidget {
+  const DotsStepper({
+    super.key,
+    this.stepCount = 3,
+    this.initialStep = 0,
+    this.confidence = 1,
+    this.activeColor = Colors.blue,
+    this.intermediateColor = Colors.orange,
+    this.color = Colors.grey,
+    this.showControls = false,
+    this.dotSize = 16.0,
+    this.onStepChanged,
+    this.controller,
+  });
+
+  final int stepCount;
+  final int initialStep;
+  final double confidence;
+  final Color activeColor;
+  final Color intermediateColor;
+  final Color color;
+  final bool showControls;
+  final double dotSize;
+  final Function(int step)? onStepChanged;
+  final ScrollController? controller;
+
+  @override
+  State<DotsStepper> createState() => _DotsStepperState();
+}
+
+class _DotsStepperState extends State<DotsStepper> {
+  int _currentStep = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _currentStep = widget.initialStep;
+    widget.controller?.addListener(_onScroll);
+  }
+
+  @override
+  void dispose() {
+    widget.controller?.removeListener(_onScroll);
+    super.dispose();
+  }
+
+  void _onScroll() {
+    if (widget.controller == null) return;
+
+    double offset = widget.controller!.offset;
+    double maxScrollExtent = widget.controller!.position.maxScrollExtent;
+    double stepWidth = maxScrollExtent / (widget.stepCount - 1);
+    int newStep = (offset / stepWidth).round();
+
+    if (newStep != _currentStep && newStep >= 0 && newStep < widget.stepCount) {
+      setState(() {
+        _currentStep = newStep;
+      });
+    }
+  }
+
+  void _decrementStep() {
+    setState(() {
+      if (_currentStep > 0) {
+        _currentStep--;
+      } else {
+        _currentStep = widget.stepCount - 1; // Go to last step
+      }
+      widget.onStepChanged?.call(_currentStep);
+    });
+  }
+
+  void _incrementStep() {
+    setState(() {
+      if (_currentStep < widget.stepCount - 1) {
+        _currentStep++;
+      } else {
+        _currentStep = 0; // Reset to first step if at the end
+      }
+      widget.onStepChanged?.call(_currentStep);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      spacing: 6,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        if (widget.showControls)
+          IconButton(
+            icon: Icon(Icons.arrow_back_ios, color: widget.color),
+            onPressed: _decrementStep,
+          ),
+        ...List.generate(widget.stepCount, (index) {
+          int difference = (_currentStep - index).abs();
+          double size = switch (index) {
+            _
+                when index >= _currentStep - widget.confidence &&
+                    index < _currentStep =>
+              widget.dotSize - (difference * 2.0),
+            _
+                when index <= _currentStep + widget.confidence &&
+                    index > _currentStep =>
+              widget.dotSize - (difference * 2.0),
+            _ when index == _currentStep => widget.dotSize,
+            _ => widget.dotSize - (widget.confidence * 2.0),
+          };
+          Color color = switch (index) {
+            _
+                when index >= _currentStep - widget.confidence &&
+                    index < _currentStep =>
+              widget.intermediateColor,
+            _
+                when index <= _currentStep + widget.confidence &&
+                    index > _currentStep =>
+              widget.intermediateColor,
+            _ when index == _currentStep => widget.activeColor,
+            _ => widget.color,
+          };
+          return Dot(size: size, color: color);
+        }),
+        if (widget.showControls)
+          IconButton(
+            icon: Icon(Icons.arrow_forward_ios, color: widget.color),
+            onPressed: _incrementStep,
+          ),
+      ],
+    );
+  }
+}
+
+class Dot extends StatelessWidget {
+  const Dot({super.key, required this.size, required this.color});
+
+  final double size;
+  final Color color;
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedContainer(
+      duration: const Duration(milliseconds: 200),
+      width: size,
+      height: size,
+      decoration: BoxDecoration(
+        color: color,
+        shape: BoxShape.circle,
+      ),
+    );
+  }
+}
--- lib/features/modules_carousel.dart
+++ lib/features/modules_carousel.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:udsp59/entities/module.dart';
 import 'package:udsp59/features/modules_carousel_element.dart';
+import 'package:udsp59/features/dots_stepper.dart';
 import 'package:udsp59/styles/form_factor.dart';
 
-class ModulesCarousel extends StatelessWidget {
+class ModulesCarousel extends StatefulWidget {
   final List<Module> modules;
 
   const ModulesCarousel({
@@ -11,22 +12,64 @@ class ModulesCarousel extends StatelessWidget {
     required this.modules,
   });
 
-  // This widget return a list of ModuleListElementWidget scrollable horizontally
+  @override
+  State<ModulesCarousel> createState() => _ModulesCarouselState();
+}
+
+class _ModulesCarouselState extends State<ModulesCarousel> {
+  final ScrollController _scrollController = ScrollController();
+
+  void _onStepChanged(int step) {
+    _scrollController.animateTo(
+      (190 * step)
+          .toDouble(), // 190 is the approximate width of each module + padding
+      duration: const Duration(milliseconds: 300),
+      curve: Curves.easeInOut,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: MediaQuery.of(context).size.width < FormFactor.biggerThanTablet
-          ? double.infinity
-          : FormFactor.biggerThanTablet,
-      height: 140,
-      child: ListView.builder(
-        padding: const EdgeInsets.all(10),
-        itemCount: modules.length,
-        scrollDirection: Axis.horizontal,
-        itemBuilder: (context, index) {
-          return ModulesCarouselElement(module: modules[index]);
-        },
-      ),
+    return Column(
+      children: [
+        SizedBox(
+          width: MediaQuery.of(context).size.width < FormFactor.biggerThanTablet
+              ? double.infinity
+              : FormFactor.biggerThanTablet,
+          height: 140,
+          child: ListView.builder(
+            controller: _scrollController,
+            padding: const EdgeInsets.all(10),
+            itemCount: widget.modules.length,
+            scrollDirection: Axis.horizontal,
+            itemBuilder: (context, index) {
+              return ModulesCarouselElement(module: widget.modules[index]);
+            },
+          ),
+        ),
+        if (widget.modules.length > 1)
+          Padding(
+            padding: const EdgeInsets.only(top: 8),
+            child: DotsStepper(
+              stepCount: widget.modules.length,
+              initialStep: 0,
+              confidence: 0,
+              color: Colors.white.withValues(alpha: 0.4),
+              activeColor: Colors.white,
+              intermediateColor: Colors.white.withValues(alpha: 0.7),
+              showControls: true,
+              dotSize: 8.0,
+              onStepChanged: _onStepChanged,
+              controller: _scrollController,
+            ),
+          ),
+      ],
     );
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
   }
 }
